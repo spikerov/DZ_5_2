ca3b8c3410f5ebee2be7b5c1a53c90dcfa39dddb
spikerov <56309248+spikerov@users.noreply.github.com>
2022-06-14 10:26:18 +0300

    Update Spawner.cs

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,23 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private Enemy _enemyPrefab;
     [SerializeField] private List<Transform> _spawnPoints;
+    [SerializeField] private Queue<Transform> _queueSpawnPoints = new Queue<Transform>();
+
+    private float _timeAfterSpawn = 0;
+    private float _timeBetweenSpawnEnemy = 2;
 
     private void Start()
     {
-        StartCoroutine(EnemySpawner());
+        TransferListToQuewe();
     }
 
-    private IEnumerator EnemySpawner()
+    private void Update()
+    {
+        _timeAfterSpawn += Time.deltaTime;
+
+        while (_timeAfterSpawn >= _timeBetweenSpawnEnemy && _queueSpawnPoints != null)
+        {
+            _timeAfterSpawn -= _timeBetweenSpawnEnemy;
+            EnemySpawner();
+        }
+    }
+
+    private void TransferListToQuewe()
     {
         for (int i = 0; i < _spawnPoints.Count; i++)
         {
-            Instantiate(_enemyPrefab, _spawnPoints[i].transform);
-            yield return new WaitForSeconds(2);
+            _queueSpawnPoints.Enqueue(_spawnPoints[i]);
         }
+    }
+
+    private void EnemySpawner()
+    {
+        Instantiate(_enemyPrefab, _queueSpawnPoints.Dequeue());
     }
 }
